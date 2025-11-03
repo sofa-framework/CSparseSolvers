e629d03369ced29d76dce1bbae3da0219172875e
hugtalbot <hugo.talbot@sofa-framework.org>
2025-11-03 20:41:12 +0100

    Apply new factory registration mechanism

--- src/CSparseSolvers/init.cpp
+++ src/CSparseSolvers/init.cpp
@@ -2,6 +2,13 @@
 
 #include <sofa/core/ObjectFactory.h>
 using sofa::core::ObjectFactory;
+#include <sofa/helper/system/PluginManager.h>
+
+namespace csparsesolvers
+{
+    extern void registerSparseLUSolver(sofa::core::ObjectFactory* factory);
+    extern void registerSparseCholeskySolver(sofa::core::ObjectFactory* factory);
+}
 
 extern "C" {
     SOFA_CSPARSESOLVERS_API void initExternalModule();
@@ -9,7 +16,7 @@ extern "C" {
     SOFA_CSPARSESOLVERS_API const char* getModuleVersion();
     SOFA_CSPARSESOLVERS_API const char* getModuleLicense();
     SOFA_CSPARSESOLVERS_API const char* getModuleDescription();
-    SOFA_CSPARSESOLVERS_API const char* getModuleComponentList();
+    SOFA_CSPARSESOLVERS_API void registerObjects(sofa::core::ObjectFactory* factory);
 }
 
 void initExternalModule()
@@ -17,18 +24,21 @@ void initExternalModule()
     static bool first = true;
     if (first)
     {
+        // make sure that this plugin is registered into the PluginManager
+        sofa::helper::system::PluginManager::getInstance().registerPlugin(csparsesolvers::MODULE_NAME);
+
         first = false;
     }
 }
 
 const char* getModuleName()
 {
-    return sofa_tostring(SOFA_TARGET);
+    return csparsesolvers::MODULE_NAME;
 }
 
 const char* getModuleVersion()
 {
-    return sofa_tostring(CSPARSESOLVERS_VERSION);
+    return csparsesolvers::MODULE_VERSION;
 }
 
 const char* getModuleLicense()
@@ -41,9 +51,8 @@ const char* getModuleDescription()
     return "A set of linear solvers based on the library CSparse";
 }
 
-const char* getModuleComponentList()
+void registerObjects(sofa::core::ObjectFactory* factory)
 {
-    /// string containing the names of the classes provided by the plugin
-    static std::string classes = ObjectFactory::getInstance()->listClassesFromTarget(sofa_tostring(SOFA_TARGET));
-    return classes.c_str();
+    csparsesolvers::registerSparseLUSolver(factory);
+    csparsesolvers::registerSparseCholeskySolver(factory);
 }
